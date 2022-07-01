f1657e9818d2afa0dcc094507051e5ad8a872eac
bsdipu1988 <bsdipu1988@gmail.com>
2022-07-01 11:34:20 +0530

    'maincontain_design'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+//ngOnInit()
+export class AppComponent implements OnInit, OnDestroy {
+
+  constructor(){
+    console.log('constructor Called');
+  }
+  ngOnDestroy(): void {
+    console.log('Des Called');
+  }
+  ngOnInit(): void {
+    console.log('oninit Called');
+  }
+  
   title = 'appscss';
 }
